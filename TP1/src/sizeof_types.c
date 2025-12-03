0ec0519fb4cd20a7a4ce33a456133a9210165826
Florian-SCAT <fofo.scat.agora@gmail.com>
2025-12-03 19:51:14 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,12 +1,13 @@
 int main(){
-printf("char: %z", sizeof(char));
-printf("signed char: %z", sizeof(signed char));
-printf("unsigned char: %z", sizeof(unsigned char));
+printf("char: %zu", sizeof(char));
+printf("signed char: %zu", sizeof(signed char));
+printf("unsigned char: %zu", sizeof(unsigned char));
 
 
 
 
   return 0;}
+
 
 
 
