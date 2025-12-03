153ed7f9aab8897c2a2234d8fa27bc2bdb5879cd
Florian-SCAT <fofo.scat.agora@gmail.com>
2025-12-03 20:16:39 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int n = 5; // hauteur de la pyramide
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        // Affichage des espaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Affichage des nombres croissants
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Affichage des nombres décroissants
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n"); // nouvelle ligne après chaque niveau
+    }
+
+    printf("Génération de la pyramide terminée.\n");
+
+    return 0;
+}
 
