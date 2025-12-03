651d7e046c8241d5c83d039a5f5733c2e171f342
Florian-SCAT <fofo.scat.agora@gmail.com>
2025-12-03 20:08:22 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    int compteur = 5; // changer la valeur pour tester, doit être < 10
+    int compteur = 5; // doit être < 10
     int i, j;
 
-    for (i = 1; i <= compteur; i++) {       // boucle pour les lignes
-        for (j = 1; j <= i; j++) {          // boucle pour les colonnes
-            if (i % 2 == 0 && j % 2 == 0) { // condition pour afficher #
+    for (i = 1; i <= compteur; i++) {
+        for (j = 1; j <= i; j++) {
+            if (i % 2 == 0 && j % 2 == 0) {
                 printf("# ");
             } else {
                 printf("* ");
             }
         }
-        printf("\n"); // nouvelle ligne après chaque ligne
+        printf("\n");
     }
 
     return 0;
