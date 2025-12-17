40987e4102a72b1912a72f57a1914684d080d161
Florian-SCAT <fofo.scat.agora@gmail.com>
2025-12-17 19:41:19 +0100

    Update repertoire.h

--- TP5/src/repertoire.h
+++ TP5/src/repertoire.h
@@ -1,10 +1,13 @@
 #ifndef REPERTOIRE_H
 #define REPERTOIRE_H
 
-// Exercice 5.1 : Liste simple d'un dossier
+// Exercice 5.1 : Liste simple
 void lire_dossier(const char *nom_repertoire);
 
-// Exercice 5.2 : Liste récursive d'un dossier et de ses sous-dossiers
+// Exercice 5.2 : Liste récursive
 void lire_dossier_recursif(const char *nom_repertoire);
+
+// Exercice 5.3 : Liste itérative
+void lire_dossier_iteratif(const char *nom_repertoire);
 
 #endif
